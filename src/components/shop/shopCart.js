487d3de1c8cd9f2c61ad65ec95c4329c6d9c9e92
NinaBalan <52449291+NinaBalan@users.noreply.github.com>
2020-09-02 15:44:51 +0300

    cart footer and button styles

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from  "react";
 
 import { connect } from "react-redux";
 import * as actions from "../../actions";
@@ -12,16 +12,36 @@ function CartButton({className, icon}) {
 }
 function CartContent({className, products}) {
     let count = products.lenght;
+    let productsJSX = products.map(product => <h1 key={product}>{product}</h1>);
     return (
         <div className={`${className} cart-content`}>
             <div className="cart-content__title">
                 Cart ({count})
             </div>
-            <div className="cart-content__products"></div>
-            <div className="cart-content__footer"></div>
+            <div className="cart-content__products">
+                {productsJSX}
+            </div>
+
+            <CartFooter className="cart-content__footer" products={products}/>
         </div>
     )
 }
+function CartFooter({className, products}) {
+    const price = 7.96;
+    return (
+        <div className={`${className} cart-footer`}>
+            <a className="cart-footer__checkout">
+                Checkout
+            </a>
+            <div className="cart-footer__subtotal">
+                Subtotal
+            </div>
+            <div className="cart-footer__price">
+                ${price}
+            </div>
+        </div>
+    )
+}    
 class ShopCart extends Component {
     render() {
         const { className } = this.props;
