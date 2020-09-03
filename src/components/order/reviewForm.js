7a6d84fb30bc4250bcd7c5e90e408a616385142c
NinaBalan <52449291+NinaBalan@users.noreply.github.com>
2020-09-03 09:02:23 +0300

    calculating the subtotal of an order

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -10,7 +10,8 @@ import ReviewProducts from "./reviewProducts";
 
 class ReviewForm extends Component {
     render() {
-        const { className, handleSubmit } = this.props;
+        const { className, handleSubmit, subtotal } = this.props;
+        let tax = 0.16;
 
         return (
             <form onSubmit={handleSubmit} className={`${className} review-form`}>
@@ -24,27 +25,28 @@ class ReviewForm extends Component {
 
                 <div className="review-form__line"></div>
                 <Field className="review-form__proceed"
-                onClick={() => history.push('/signin')}
-                type="submit"
-                title="Proceed to Checkout"
-                name="proceed"
-                component={FormButton}/>
+                    onClick={() => history.push('/signin')}
+                    type="submit"
+                    title="Proceed to Checkout"
+                    name="proceed"
+                    component={FormButton}/>
+
                 <Field className="review-form__back"
-                onClick={() => history.push('/shop')}
-                type="button"
-                title="Back"
-                name="back"
-                short={true}
-                component={FormButton}/>
+                    onClick={() => history.push('/shop')}
+                    type="button"
+                    title="Back"
+                    name="back"
+                    short={true}
+                    component={FormButton}/>
 
                <div className="review-form__details review-details">
                     <div className="review-details__subtotal review-detail">
                         <div className="review-detail__title">Subtotal</div>
-                        <div className="review-detail__price">$7.96</div>
+                        <div className="review-detail__price">${subtotal}</div>
                     </div>
                     <div className="review-details__subtotal review-detail">
                         <div className="review-detail__title">Tax</div>
-                        <div className="review-detail__price">$0.16</div>
+                        <div className="review-detail__price">${tax}</div>
                     </div>
                     <div className="review-details__subtotal review-detail">
                         <div className="review-detail__title">Shipping</div>
@@ -52,7 +54,7 @@ class ReviewForm extends Component {
                     </div>
                     <div className="review-details__total review-detail review-detail-green">
                         <div className="review-detail__title review-detail-green__title">Total</div>
-                        <div className="review-detail__price review-detail-green__price">$8.12</div>
+                        <div className="review-detail__price review-detail-green__price">${subtotal + tax}</div>
                     </div>
 
                 </div> 
